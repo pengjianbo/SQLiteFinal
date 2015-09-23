f5d3e50917ea674b8f97164f2a7247a76d71cfeb
彭建波 <pengjianbosoft@gmail.com>
2015-09-24 00:58:33 +0800

    提交1.0.1版本

---
+++ sqlitefinal/src/main/java/cn/finalteam/sqlitefinal/utils/IOUtils.java
@@ -0,0 +1,33 @@
+package cn.finalteam.sqlitefinal.utils;
+
+import android.database.Cursor;
+import java.io.Closeable;
+
+/**
+ * Desction:IO工具类
+ * Author:pengjianbo
+ * Date:15/7/3 上午11:04
+ */
+public class IOUtils {
+
+    private IOUtils() {
+    }
+
+    public static void closeQuietly(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (Throwable e) {
+            }
+        }
+    }
+
+    public static void closeQuietly(Cursor cursor) {
+        if (cursor != null) {
+            try {
+                cursor.close();
+            } catch (Throwable e) {
+            }
+        }
+    }
+}
